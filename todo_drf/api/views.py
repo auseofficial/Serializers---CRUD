4620d5cbbf154453cc52619ba5cba6f04493f18d
Akib Us Suny Eshan <eshan.r029@gmail.com>
2024-09-01 15:26:16 +0600

    updated form

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
 @api_view(['GET'])
 def apiOverview(request):
@@ -17,6 +15,21 @@ def apiOverview(request):
     }
     return Response(api_urls)
 
-       
-    
-    return Response("API BASE POINT", safe=False)
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
